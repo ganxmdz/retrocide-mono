4fd5c119d9ad47f207bfa480e71ed64bf0f382af
ganxmdz <vmtslave@gmail.com>
2025-12-11 01:21:43 +0000

    Add files via upload

--- Dependencies/Module.cs
+++ Dependencies/Module.cs
@@ -1,131 +1,91 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using IllusionPlugin;
+using IllusionInjector;
 using MelonLoader.Modules;
+using MelonLoader.Resolver;
 
 [assembly: MelonLoader.PatchShield]
 
 namespace MelonLoader.CompatibilityLayers
 {
-    internal class Demeo_Module : MelonModule
+    internal class IPA_Module : MelonModule
     {
-        private Dictionary<MelonBase, object> ModInformation = new Dictionary<MelonBase, object>();
-        private IList ModInfoList;
-
-        private Type modInfoType;
-        private FieldInfo name_field;
-        private FieldInfo version_field;
-        private FieldInfo author_field;
-        private FieldInfo description_field;
-        private FieldInfo isNetworkCompatible_field;
-
         public override void OnInitialize()
         {
-            MelonEvents.OnApplicationStart.Subscribe(OnPreAppStart, int.MaxValue);
-            MelonBase.OnMelonRegistered.Subscribe(ParseMelon, int.MaxValue);
-            MelonBase.OnMelonUnregistered.Subscribe(OnUnregister, int.MaxValue);
-        }
+			// To-Do:
+			// Detect if IPA is already Installed
+			// Point AssemblyResolveInfo to already installed IPA Assembly
+			// Point GetResolverFromAssembly to Dummy MelonCompatibilityLayer.Resolver
 
-        private void OnPreAppStart()
-        {
-            try
+			string[] assembly_list =
+			{
+				"IllusionPlugin",
+				"IllusionInjector",
+			};
+			Assembly base_assembly = typeof(IPA_Module).Assembly;
+			foreach (string assemblyName in assembly_list)
+                MelonAssemblyResolver.GetAssemblyResolveInfo(assemblyName).Override = base_assembly;
+
+			MelonAssembly.CustomMelonResolvers += Resolve;
+		}
+
+        private ResolvedMelons Resolve(Assembly asm)
+		{
+			IEnumerable<Type> pluginTypes = asm.GetValidTypes(x =>
+			{
+				Type[] interfaces = x.GetInterfaces();
+				return (interfaces != null) && interfaces.Any() && interfaces.Contains(typeof(IPlugin)); // To-Do: Change to Type Reflection based on Setup
+			});
+			if ((pluginTypes == null) || !pluginTypes.Any())
+				return new ResolvedMelons(null, null);
+
+			var melons = new List<MelonBase>();
+			var rotten = new List<RottenMelon>();
+			foreach (var t in pluginTypes)
             {
-                Assembly assembly = Assembly.Load("Assembly-CSharp");
-                Type moddingApi = assembly.GetType("Boardgame.Modding.ModdingAPI");
-
-                modInfoType = moddingApi.GetNestedType("ModInformation");
-                name_field = modInfoType.GetField("name", BindingFlags.Public | BindingFlags.Instance);
-                version_field = modInfoType.GetField("version", BindingFlags.Public | BindingFlags.Instance);
-                author_field = modInfoType.GetField("author", BindingFlags.Public | BindingFlags.Instance);
-                description_field = modInfoType.GetField("description", BindingFlags.Public | BindingFlags.Instance);
-                isNetworkCompatible_field = modInfoType.GetField("isNetworkCompatible", BindingFlags.Public | BindingFlags.Instance);
-
-                FieldInfo externalModsField = moddingApi.GetField("ExternallyInstalledMods", BindingFlags.Public | BindingFlags.Static);
-                if (externalModsField.GetValue(null) == null)
-                {
-                    var listType = typeof(List<>);
-                    var constructedListType = listType.MakeGenericType(modInfoType);
-                    ModInfoList = (IList)Activator.CreateInstance(constructedListType);
-                    externalModsField.SetValue(null, ModInfoList);
-                }
-
-                foreach (var m in MelonPlugin.RegisteredMelons)
-                {
-                    try
-                    {
-                        ParseMelon(m);
-                    }
-                    catch (Exception ex)
-                    {
-                        MelonLogger.Error($"Demeo Integration has thrown an exception: {ex}");
-                    }
-                }
-
-                foreach (var m in MelonMod.RegisteredMelons)
-                {
-                    try
-                    {
-                        ParseMelon(m);
-                    }
-                    catch (Exception ex)
-                    {
-                        MelonLogger.Error($"Demeo Integration has thrown an exception: {ex}");
-                    }
-                }
+				var mel = LoadPlugin(asm, t, out RottenMelon rm);
+				if (mel != null)
+					melons.Add(mel);
+				else
+					rotten.Add(rm);
             }
+			return new ResolvedMelons(melons.ToArray(), rotten.ToArray());
+		}
+
+		private MelonBase LoadPlugin(Assembly asm, Type pluginType, out RottenMelon rottenMelon)
+		{
+			rottenMelon = null;
+			IPlugin pluginInstance;
+			try
+			{ pluginInstance = Activator.CreateInstance(pluginType) as IPlugin; }
             catch (Exception ex)
             {
-                MelonLogger.Error($"Demeo Integration has thrown an exception: {ex}");
+				rottenMelon = new RottenMelon(pluginType, "Failed to create a new instance of the IPA Plugin.", ex);
+				return null;
             }
-        }
 
-        private void OnUnregister(MelonBase melon)
-        {
-            if (melon == null)
-                return;
+			MelonProcessAttribute[] processAttrs = null;
+			if (pluginInstance is IEnhancedPlugin enPl)
+				processAttrs = enPl.Filter?.Select(x => new MelonProcessAttribute(x)).ToArray();
 
-            if (!ModInformation.ContainsKey(melon))
-                return;
+			string pluginName = pluginInstance.Name;
+			if (string.IsNullOrEmpty(pluginName))
+				pluginName = pluginType.FullName;
 
-            try
-            {
-                object info = ModInformation[melon];
-                ModInformation.Remove(melon);
-                ModInfoList.Remove(info);
-            }
-            catch (Exception ex)
-            {
-                MelonLogger.Error($"Demeo Integration has thrown an exception: {ex}");
-            }
-        }
+			string plugin_version = pluginInstance.Version;
+			if (string.IsNullOrEmpty(plugin_version))
+				plugin_version = asm.GetName().Version.ToString();
+			if (string.IsNullOrEmpty(plugin_version) || plugin_version.Equals("0.0.0.0"))
+				plugin_version = "1.0.0.0";
 
-        private void ParseMelon<T>(T melon) where T : MelonBase
-        {
+			var melon = MelonBase.CreateWrapper<IPAPluginWrapper>(pluginName, null, plugin_version, processes: processAttrs);
 
-            if (melon == null)
-                return;
-
-            if (ModInformation.ContainsKey(melon))
-                return;
-
-            try
-            {
-                object info = Activator.CreateInstance(modInfoType);
-
-                name_field.SetValue(info, melon.Info.Name);
-                version_field.SetValue(info, melon.Info.Version);
-                author_field.SetValue(info, melon.Info.Author);
-                description_field.SetValue(info, melon.Info.DownloadLink);
-                isNetworkCompatible_field.SetValue(info, MelonUtils.PullAttributeFromAssembly<Demeo_LobbyRequirement>(melon.MelonAssembly.Assembly) == null);
-
-                ModInformation.Add(melon, info);
-                ModInfoList.Add(info);
-            }
-            catch (Exception ex)
-            {
-                MelonLogger.Error($"Demeo Integration has thrown an exception: {ex}");
-            }
-        }
-    }
+			melon.pluginInstance = pluginInstance;
+			PluginManager._Plugins.Add(pluginInstance);
+			return melon;
+		}
+	}
 }
